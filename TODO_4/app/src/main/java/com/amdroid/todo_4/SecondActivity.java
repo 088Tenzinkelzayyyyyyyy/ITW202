50f508a403a99bb9e31eb2963f0a5e35338d3879
088Tenzinkelzayyyyyyyy <12190088.gcit@rub.edu.bt>
2021-05-26 14:56:23 +0600

    TODO

--- TODO_4/app/src/main/java/com/amdroid/todo_4/SecondActivity.java
+++ TODO_4/app/src/main/java/com/amdroid/todo_4/SecondActivity.java
@@ -1,37 +1,41 @@
-package bt.edu.todo_4;
+ package com.amdroid.todo_4;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.amdroid.todo_4.R;
 
-public class SecondActivity extends AppCompatActivity {
-    public static final String EXTRA_REPLY = "bt.edu.todo_4";
+ public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
     private EditText mReply;
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
         mReply = findViewById(R.id.editText_second);
-
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
+        String message = intent.getStringExtra(com.amdroid.todo_4.MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
     }
 
-    public void Reply(View view) {
+    public void returnReply(View view) {
         String reply = mReply.getText().toString();
-
         Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
+        replyIntent.putExtra(EXTRA_REPLY, reply);  
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
+
 }
